65773366def4df273cdd2ffc8b723e022fefb0df
bricecorreia <rednex.dev@gmail.com>
2022-05-09 17:15:44 +0200

    fix & refacto exo3

--- exo3/DrinkExpense.php
+++ exo3/DrinkExpense.php
@@ -1,62 +1,11 @@
 <?php
 
-class DrinkExpense
+require_once('Expense.php');
+
+class DrinkExpense extends Expense
 {
-    private float $amount;
-
-    private $description;
-
-    private \DateTime $happenedAt;
-
-    private User $le_payeur;
-
-    /**
-     * @var array<User>
-     */
-    private array $participants;
-
-    /**
-     * @param array <string, User> $participants
-     */
-    public function __construct(float $amount, string $description, DateTime $happenedAt, User $le_payeur, array $participants)
+    function getType()
     {
-        $this->amount = $amount;
-        $this->description = $description;
-        $this->happenedAt = $happenedAt;
-        $this->le_payeur = $le_payeur;
-        $this->participants = $participants;
-    }
-
-
-    /**
-     * @return array<string, User> $participants
-     */
-    public function getParticipants(): array
-    {
-        return $this->participants;
-    }
-
-    public function getAmount(): float
-    {
-        return $this->amount;
-    }
-
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    public function getHappenedAt(): \DateTime
-    {
-        return $this->happenedAt;
-    }
-
-    public function getPayer(): User
-    {
-        return $this->le_payeur;
-    }
-
-    function get_type() {
         return 'DRINK';
     }
 }
--- exo3/FoodExpense.php
+++ exo3/FoodExpense.php
@@ -1,61 +1,11 @@
 <?php
 
-class FoodExpense
+require_once('Expense.php');
+
+class FoodExpense extends Expense
 {
-    private float $amount;
-
-    private $description;
-
-    private \DateTime $happenedAt;
-
-    private User $le_payeur;
-
-    /**
-     * @var array<User>
-     */
-    private array $participants;
-
-    /**
-     * @param array <string, User> $participants
-     */
-    public function __construct(float $amount, string $description, DateTime $happenedAt, User $le_payeur, array $participants)
+    function getType()
     {
-        $this->amount = $amount;
-        $this->description = $description;
-        $this->happenedAt = $happenedAt;
-        $this->le_payeur = $le_payeur;
-        $this->participants = $participants;
-    }
-
-    public function getAmount(): float
-    {
-        return $this->amount;
-    }
-
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    public function getHappenedAt(): \DateTime
-    {
-        return $this->happenedAt;
-    }
-
-    public function getPayer(): User
-    {
-        return $this->le_payeur;
-    }
-
-    /**
-     * @return array<string, User> $participants
-     */
-    public function getParticipants(): array
-    {
-        return $this->participants;
-    }
-
-    function get_type() {
         return 'FOOD';
     }
 }
